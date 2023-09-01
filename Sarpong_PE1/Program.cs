725fc09ed88bd11c3ad24e4f7306df766f446642
Nana Sarpong <ns8363@rit.edu>
2023-08-31 21:40:07 -0400

    PE1 - "Hellow World"

---
+++ Sarpong_PE1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Sarpong_PE1
+{
+    // the name of our class which is set to a default name called Program
+    static internal class Program
+    {
+        // our method called Main which allows our code to be processed 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nana Sarpong");
+
+            // using "let" in C# is not possible as it is the wrong syntax for this program
+            // console.log doesn't work either, but .WriteLine is the same function
+
+            string sMyName = "";
+            Console.Write("What Is Your name?");
+
+            // ReadLine needs to be initialized to a variable
+            sMyName = Console.ReadLine();
+            bool bMyNameIsNana = true;
+
+            if (bMyNameIsNana == true)
+            {
+                Console.WriteLine("My Middle name is Kofi");
+            }
+
+
+            // for loops still work regularly
+            // still need to replace "let" with int
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
