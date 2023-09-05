473cc4ce14588af48a79c1de1a822b018ccf390c
Nana Sarpong <ns8363@rit.edu>
2023-09-05 09:05:10 -0400

    Session 2 - 1

--- FavoriteColorsAnd Numbers/Program.cs
+++ FavoriteColorsAnd Numbers/Program.cs
@@ -112,12 +112,132 @@ namespace FavoriteColorAndNumber
                 catch
                 {
                     Console.WriteLine("Please enter an integer: ");
+                    sFavoriteColor = Console.ReadLine();
                 }
             }
-           
-              
+
+            do
+            {
+                sFavoriteColor = Console.ReadLine();
+                try
+                {
+                    nFavoriteNumber = Convert.ToInt32(sFavoriteNumber);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer: ");
+                    sFavoriteColor = Console.ReadLine();
+                }
+            } while (nFavoriteNumber == null);
+
+
+            do
+            {
+                sFavoriteColor = Console.ReadLine();
+                try
+                {
+                    sFavoriteNumber = "42";
+                    // sFavoriteNumber + sFavoriteNumber != 84 ("4242")
+                   // sFavoriteNumber.Length = 2;
+                    nFavoriteNumber =int.Parse(sFavoriteNumber);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer: ");
+                    sFavoriteColor = Console.ReadLine();
+                }
+            } while (nFavoriteNumber == null);
+
+
+
+            int nFavoriteNumberInt = 0;
+            do
+            {
+                sFavoriteColor = Console.ReadLine();
+                
+                    // will convert the string variable to the interger dataype, which will come out as the integer variable set (out variable)
+                    bValid = int.TryParse(sFavoriteNumber, out nFavoriteNumberInt);
+
+                if (bValid == false) // same thing
+                if (!bValid)
+                {
+
+                }
+                
+                
+                {
+                    Console.WriteLine("Please enter an integer: ");
+                  
+                }
+            } while (nFavoriteNumber == null);
+            nFavoriteNumber = nFavoriteNumberInt;
+
+
+
+
+
+            {
+                int myInt2 = 0;
+
+                myInt = 3;
+                myInt = 5;
+                string myString = "";
+            }
+            //   myString = "David"; - not in code block
+
+
+            // set favorite color to lower case
+            // this won't change the string contents
+            sFavoriteColor.ToLower();
+
+
+            // change the console output color to match their favorite color
+            switch (sFavoriteColor.ToLower())
+            {
+                case "red":
+               // case "RED":
+               // case "Red":
+               // case "rEd":
+              //  case "reD":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+
+
+
+
+
+
+            int y = 0;
+            int x = 5;
+           // y = ++x; // increment x so that x = 6, y = 6
+          //  y = x++; // y = 5, x = 6
 
             // a loop that outputs their favorite color fav num times
+            for (int i = 0; i < nFavoriteNumber; i++)
+            {
+                Console.WriteLine("Your favorite color is " + sFavoriteColor + "!");
+                Console.WriteLine($"Your favorite color is {sFavoriteColor +  "!"}"); // string interpolation
+                Console.WriteLine("Your {0} favorite color is {1}{2}", "most", sFavoriteColor, "!");
+
+                Console.WriteLine("5 + 5 = " + (x + x)); // 5 + 5 = 10
+            }
+
+
+
+
         }
     }
 }
---
+++ Sarpong_PE3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sarpong_PE3
+{
+
+    namespace fabulous
+    {
+        // code in fabulous namespace
+       
+    }
+
+    namespace super
+    {
+        namespace smashing
+        {
+            
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int num1 = 2;
+            string number = "3.50";
+            int num2 = 4;
+            int num3 = 5;
+            num1 = num1 * num3;
+            num2 = num1 / num2;
+            num3 = num3 % num2;
+
+            Console.Out.WriteLine("50 plus 25 is " + 50 + 25);
+        }
+    }
+}
