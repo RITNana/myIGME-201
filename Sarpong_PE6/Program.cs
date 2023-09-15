0134ec78c7840bb33e61e9159bf4a3fb93e37efe
Nana Sarpong <ns8363@rit.edu>
2023-09-14 22:42:38 -0400

    PE6 assignment

--- Sarpong_PE6/Program.cs
+++ Sarpong_PE6/Program.cs
@@ -3,59 +3,79 @@
 
 namespace Sarpong_PE6
 {
+    // Nana Sarpong
+    /* Create a simple number guessing game. Your program will generate a random integer number between
+     0 and 100 (inclusive) and a player will try to guess it. */ 
     internal class Program
     {
+        // hold and compile code for program to run
         static void Main(string[] args)
         {
+            Random rand = new Random(); // create a new random object
+            int randomNumber = rand.Next(0, 101); // set bounds for random numbers
 
+             Console.WriteLine(randomNumber); // output random number
 
+            int tries = 0; // number of tries user takes
+            string sNumber = ""; // string input of number
+            int nNumber = 0; // interger output of string input
+            bool bValid; // testing if valid
 
-            Random rand = new Random();
-
-            // generate a number from 0 (inclusive) to 101 (exclusive)
-            int randomNumber = rand.Next(0, 101);
-             Console.WriteLine(randomNumber);
-
-            // generate a loop that runs for 8 tries for the user
-            for (int i = 0; i <= 8; i++)
+            for (tries = 1; tries <= 8; tries++)
             {
-                string sNumber = "";
-                int nNumber = 0;
-                bool bValid = false;
-              //  int nCorrect = 0;
-
-                // valid  and invalid entries
-                int counterGood = 0;
-                int counterBad = 0;
-                
-                do
+                do // run code while boolean prompt stands
                 {
-                    Console.WriteLine("Guess a number");
-                    sNumber = Console.ReadLine(); // read user input and convert to integer
+                    Console.WriteLine("Guess the number between 1 and 100"); // prompt the user to guess a number between 1 to 100
+                    sNumber = Console.ReadLine(); // read user input
+                    bValid = int.TryParse(sNumber, out nNumber); // if valid, will convert user's string number to an integer
+
                     if (!bValid)
                     {
-                        if (int.TryParse(sNumber, out nNumber))
-                        {
-                            if (nNumber >= 0 && nNumber <= 100)
-                            {
-                                if (nNumber == randomNumber)
-                                {
-                                    Console.WriteLine("Good Job! You got the number!");
-                                    counterGood++;
-                                } else
-                                {
-                                    counterBad++;
-                                }  
-                            } else 
-                            {
-                                Console.WriteLine("Invalid Answer, try again");
-                                sNumber = Console.ReadLine();
-                                counterBad++;
-                            }
-                            
-                        }
+                        Console.WriteLine("Please enter a VALID number"); // if not valid, will send a message (won't take away turn)
+                        
+                    }
+                } while (!bValid); 
+                if ((nNumber >= 0) && (nNumber < 100)) // if between the ranges
+                {
+                    if (nNumber < randomNumber) // if number guessed is less than the randomNumber
+                    {
+                        Console.WriteLine("Your guess was too low");
+                      
+                    }
+                    else if (nNumber > randomNumber) // if number guessed is greater than randomNumber
+                    {
+                        Console.WriteLine("Your guess was too high");
+                     
+                    }
+                    else if (nNumber == randomNumber) // if they got the randomNumber right
+                    {
+                        Console.WriteLine("Correct!");
+
+                    }
+                    if ((nNumber == randomNumber) && (tries < 8)) // if they got it right in less than 8 tries
+                    {
+                        Console.WriteLine($"You won in {tries} turns ");
+                        break; // stop code once they got it right in less than 8 tries
+                    }
+                }
+
+               
+
 
             }
+            // if they didn't get it right and used up all theri turns, state correct answer and end game
+            if ((nNumber < randomNumber) || (nNumber > randomNumber) || (tries == 8)) 
+            {
+                Console.WriteLine("I'm sorry, the answer is: " + randomNumber);
+
+            } else
+            {
+
+            }
+
+
+
+
         }
     }
 }
--- Sarpong_PE6/Properties/AssemblyInfo.cs
+++ Sarpong_PE6/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c7b7573-30e7-479e-b248-7156696982cf")]
+[assembly: Guid("9e51408a-de6c-4926-93b3-8a29eb4a8d32")]
 
 // Version information for an assembly consists of the following four values:
 //
