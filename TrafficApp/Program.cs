114b0e20f3384499afb723e948993d89e9561362
Nana Sarpong <ns8363@rit.edu>
2023-10-10 21:11:52 -0400

    PE11 - Question 6

--- TrafficApp/Program.cs
+++ TrafficApp/Program.cs
@@ -9,8 +9,13 @@ using VehiclesLib;
 
 namespace TrafficApp
 {
+    // Nana Sarpong
+    // PE11 - Question 6
+    // Create a console application project, Traffic, which references Vehicles.dll 
+    // Errors: none
     static internal class Program 
     {
+        // declare an interface object set equal to a new class that is DERIVED from the interface IPassengerCarrier
         static void Main(string[] args)
         {
             //Vehicle.Car.Compact carone = new Vehicle.Car.Compact();
@@ -23,11 +28,12 @@ namespace TrafficApp
             
         }
 
+        // vehicleObj is a paramater for the interface object from IPassengerCarrier
          static void AddPassenger(IPassengerCarrier vehicleObj)
         {
-            vehicleObj.LoadPassenger();
-            Console.WriteLine("Works");
-            Console.WriteLine(vehicleObj.ToString());
+            vehicleObj.LoadPassenger(); // call the method from the interface
+            Console.WriteLine("Works"); // test to see if reference worked
+            Console.WriteLine(vehicleObj.ToString()); // call toString() from System.Object
             
         }
     }
